f40f192ebc54fcf9ab919edefa841b9f2ca3f0ec
malemuller <mmuller@itba.edu.ar>
2019-08-14 01:21:00 -0300

    Lee excel. Devuelve nombres de las columnas

--- utilities/data_readers/csv_reader.py
+++ utilities/data_readers/csv_reader.py
@@ -2,12 +2,12 @@
 TO DO:
     (1) Definir si usamos numero o nombre de columna para acceder a la misma.
     (2) VERIFICAR QUE LA COLUMNA EXISTA, YA SEA NUMERO O NOMBRE.
-    (3) Ver pip install xlrd para importar datos del excel.
     (4) Averiguar paths.
     (5) Agregar la parte de ltspice.
     (6) Ver si input data se guarda en algún lado o no.
     (7) Agregar caso .txt.
-    (8) Ver si meter esto en una clase data_input o similar.
+    (8) Meter todo esto en una clase DataReader o similar y que guarde data solo la primera vez que lo abre para buscar las cols.
+
 '''
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,9 +17,34 @@ import csv
 import os
 
 
-DEBUG = True
+DEBUG = False
 
 
+def get_file_data_names(file_path):
+    '''
+    This function receives a data-stored-file's path and returns the names
+    of its data columns.
+    :param file_path: Path of the file from which data names will be returned.
+    :return: The names of each of the data columns.
+    '''
+    base = os.path.basename(file_path)
+    if os.path.splitext(base)[1] == ".csv":
+        data = pd.read_csv(file_path, delimiter=";", encoding="utf-8", decimal=",")
+        print(data)
+    elif os.path.splitext(base)[1] == ".txt":
+        print("es un txt")
+        data = pd.read_csv(file_path, sep='[,\t]', encoding="latin_1", decimal=".")
+        print(data)
+    elif (os.path.splitext(base)[1] == ".xls") or (os.path.splitext(base)[1] == ".xlsx"):
+        data = pd.read_excel('xlsTest.xlsx')#, sheetname='Sheet1')
+        #print("Column headings:")
+        #print(df.columns)
+        print(data)
+    #data_names = list(data.columns)
+    data_names = list(data.columns.values) #returns an array of index
+    #data_names = list(data.columns.values.tolist())
+    print(data_names)
+    return(data_names)
 
 def import_file_data(file_path, x_col_name, x_col_number, y_col_name, y_col_number):
     '''
@@ -41,10 +66,11 @@ def import_file_data(file_path, x_col_name, x_col_number, y_col_name, y_col_numb
         print("es un txt")
         data = pd.read_csv(file_path, sep='[,\t]', encoding="latin_1", decimal=".")
         print(data)
-    #elif (os.path.splitext(base)[1] == ".xls") or (os.path.splitext(base)[1] == ".xlsx"):
-        #data = pd.read_excel('xlsTest.xlsx', sheetname='Sheet1')
+    elif (os.path.splitext(base)[1] == ".xls") or (os.path.splitext(base)[1] == ".xlsx"):
+        data = pd.read_excel('xlsTest.xlsx')#, sheetname='Sheet1')
         #print("Column headings:")
         #print(df.columns)
+        print(data)
     #else:
     #    print("Incorrect file extension")
     '''
@@ -56,24 +82,26 @@ def import_file_data(file_path, x_col_name, x_col_number, y_col_name, y_col_numb
     data.sort_values(by=x_col_name, axis='index', inplace=True, ascending=True, na_position='last')
     data = data.reset_index(drop=True)
     # --- TO DO (1): Dependiendo de si trabajamos con nombre o numero de fila, determinar si usamos formato x o x2
-    x = data.loc[:, x_col_name]
-    x2 = data.loc[:, data.columns[x_col_number]]
-    y = data.loc[:, y_col_name]
-    y2 = data.loc[:, data.columns[y_col_number]]
+   # x = data.loc[:, x_col_name]
+    x = data.loc[:, data.columns[x_col_number]]
+    #y = data.loc[:, y_col_name]
+    y = data.loc[:, data.columns[y_col_number]]
     # --- (HASTA ACA (1))
     return (x,y)
 
 def main(): #FOR TEST
     #file_path = "xlsTest.xlsx"
-    file_path = "txtTest2.txt"
-    #file_path = "csvTest.csv"
-    x_col_name = "Freq."
+    #file_path = "txtTest2.txt"
+    file_path = "csvTest.csv"
+    #x_col_name = "Freq."
+    x_col_name = "FREQUENCY"
     x_col_number = 0
     #y_col_name = "Amplitude"
     y_col_name = "I(L1)"
     y_col_number = 1
 
-    input_data = import_file_data(file_path, x_col_name, x_col_number, y_col_name, y_col_number)
+    data_columns = get_file_data_names(file_path)
+    #input_data = import_file_data(file_path, x_col_name, x_col_number, y_col_name, y_col_number)
     if DEBUG:
         print("data received:")
         print(input_data[0])
